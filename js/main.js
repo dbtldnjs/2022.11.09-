5613e7ded9ce667246e4f7e5f45aefe16fb9306c
you19721@naver.com <dbtldnjs>
2022-11-09 21:51:24 +0900

    웹팩01

---
+++ js/main.js
@@ -0,0 +1,4 @@
+console.log('welcome')
+import '../css/index.css'
+import '../scss/index.scss'
+
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path')
+const htmlPlugin = require('html-webpack-plugin')
+const copyplugin = require('copy-webpack-plugin')
+module.exports = {
+    // 시작점
+entry:'./js/main.js',
+
+    // 결과문의 반환하는 패스 설정
+output:{
+// path:path.resolve(__dirname,'dist'),
+// filename:'main.js',
+clean:true
+},
+    // module
+module:{
+    rulres:[
+        
+        {
+            test:/\.s?css$/,
+            use:[
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ]
+        },
+        {
+            test:/\.js$/,
+            exclude:/node_modules/,
+            use:['babel-loader']
+        }
+    ]
+},
+
+plugins:[
+    new htmlPlugin({
+        template:'./index.html'
+    }),
+    new copyplugin(
+        {pateerns:[
+            {from:'static'}
+        ]}
+    )
+]
+    // plugins
+}
